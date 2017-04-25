c7a4fddc6bd316c28efe5ad7fa2adba0124ed82f
abhishekpandya24 <abhishek.pandya@xoriant.com>
2017-04-25 11:13:27 +0530

    Jquery implementation

---
+++ src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,28 @@
+﻿//site.js
+(function () {
+    //var ele = $("#username");
+    //ele.text( "Abhishek Pandya");
+
+    //var main = $("#main");
+    //main.on("mouseenter", function () {
+    //    main.style = "backgroundColor :#888";
+    //});
+
+    //main.on("mouseleave" , function () {
+    //    main.style = "";
+    //});
+
+    //var menuItems = $("ul.menu li a");
+    //menuItems.on("click", function () {
+    //    var me = $(this);
+    //    alert(me.text());
+    //})
+
+    var sideBarAndWrapper = $("#sidebar,#wrapper");
+
+    $("#sidebarToggle").on("click", function () {
+        sideBarAndWrapper.toggleClass("hide-sidebar");
+    });
+    
+
+})();
